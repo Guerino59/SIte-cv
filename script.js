5f286d3c8a70b4acbfaebb6c21997c8afa5f0b3a
unknown <aaa@gmail.com>
2022-11-10 16:27:44 +0100

    modif

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 
+"use strict";
+
+
+
+document.querySelector('nav button').forEach(item => {
+    item.addEventListener('click', second);
+})
+
+function second() {
+    let body = document.querySelector('body');
+    let mode = this.dataset.mode;
+    body.dataset.theme = mode;
+  }
