aee41a9cfa352496d03ec66ad646bbcb6994103a
unknown <aaa@gmail.com>
2022-11-15 16:07:55 +0100

    color

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 
 "use strict";
 
-
-
-const btn = document.querySelector('nav button');
+const btn = document.querySelector('button');
+console.log(btn);
 
 btn.addEventListener('click', second);
 
 
 function second() {
     let body = document.querySelector('body');
-    let mode = this.dataset.mode;
-    body.dataset.theme = mode;
+    if(body.dataset.theme == "main")
+    {
+      body.dataset.theme = "second"
+    }
+    else if(body.dataset.theme == "second")
+    {
+      body.dataset.theme = "third"
+    }
+    else if(body.dataset.theme == "third")
+    {
+      body.dataset.theme = "fourth"
+    }
+    else{
+      body.dataset.theme = "main"
+    }
   }
